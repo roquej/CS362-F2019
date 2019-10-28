0b9223907a0c7c0b5b10b16f3030309ae02e4cfd
Jonasel Roque <jonaselroque@me.com>
2019-10-27 21:31:19 -0700

    Fixed error noted by TA where previous push had code refactoring in the wrong folder

--- projects/roquej/dominion/dominion_helpers.h
+++ projects/roquej/dominion/dominion_helpers.h
@@ -11,5 +11,9 @@ int gainCard(int supplyPos, struct gameState *state, int toFlag, int player);
 int getCost(int cardNumber);
 int cardEffect(int card, int choice1, int choice2, int choice3,
                struct gameState *state, int handPos, int *bonus);
-
+int playBaron(int choice1, int currentPlayer, struct gameState *state);
+int playMinion(int choice1, int choice2, int handPos, int currentPlayer, struct gameState *state);
+int playAmbassador(int choice1, int choice2, int currentPlayer, int handPos, struct gameState *state);
+int playTribute(int *tributeRevealedCards, int currentPlayer, int nextPlayer, struct gameState *state);
+int playMine(int choice1, int choice2, int currentPlayer, int handPos, struct gameState *state);
 #endif
