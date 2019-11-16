2ea155f61d27761dd8b456ab4f9b9c3fc959df6e
Jonasel Roque <jonaselroque@me.com>
2019-11-16 11:50:00 -0800

    checking in random testers and edited makefile

---
+++ projects/roquej/dominion/randomtestcard2.c
@@ -0,0 +1,14 @@
+// Jonasel Roque
+// roquej@oregonstate.edu
+// CS362-F2019
+// random test generator- minion card
+
+#include <stdio.h>
+#include <string.h>
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include "rngs.h"
+
+int main(int argc, char* argv[]) {
+    return 0;
+}
