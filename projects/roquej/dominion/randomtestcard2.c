b15ddfca04ae3546c4016a89f5de7c3f4429448a
Jonasel Roque <jonaselroque@me.com>
2019-11-17 23:43:30 -0800

    implemented random testing and edited makefile

--- projects/roquej/dominion/randomtestcard2.c
+++ projects/roquej/dominion/randomtestcard2.c
@@ -4,11 +4,78 @@
 // random test generator- minion card
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include "dominion.h"
 #include "dominion_helpers.h"
 #include "rngs.h"
 
 int main(int argc, char* argv[]) {
+    printf("Begin random testing of minion card:\n");
+
+    srand(time(NULL));
+    
+    // declare variables
+    int choice1, choice2, currentPlayer, numPlayers, 
+        preCoins, postCoins, preActions, postActions;
+    int wrongActions = 0;
+    int wrongCoins = 0;
+    int wrongHandCount = 0;
+    struct gameState g0, g;
+    int k[10] = {minion, council_room, feast, gardens, mine,
+                 remodel, smithy, village, adventurer, great_hall};
+    for(int i = 0; i < 1000; i++) {
+        memcpy(&g, &g0, sizeof(struct gameState));
+    
+        numPlayers = 2 + rand() % 3;
+        initializeGame(numPlayers, k, 500, &g);
+        currentPlayer = rand() % numPlayers;
+
+        if(rand() % 100 < 50) {
+            choice1 = 1;
+            choice2 = 0;
+        } else {
+            choice1 = 0;
+            choice2 = 1;
+        }
+
+        gainCard(minion, &g, 2, currentPlayer);
+
+        preCoins = g.coins;
+        preActions = g.numActions;
+
+        for(int j = 0; j < 5; j++) {
+            drawCard(currentPlayer, &g);
+        }
+
+        playMinion(choice1, choice2, 0, currentPlayer, &g);
+
+        postCoins = g.coins;
+        postActions = g.numActions;
+        
+        if(preActions + 1 != postActions) {
+            wrongActions++;
+        }
+
+        if(choice1) {
+            if(preCoins + 2 != postCoins) {
+                wrongCoins++;
+            }
+        } else {
+            if(preCoins != postCoins) {
+                wrongCoins++;
+            }
+            if(g.handCount[currentPlayer] != 4) {
+                wrongHandCount++;
+            }
+        }
+    }
+
+    printf("In 1000 iterations:\n");
+    printf("%d iterations had an inappropriate change in actions.\n", wrongActions);
+    printf("%d iterations had an inappropriate change in coins.\n", wrongCoins);
+    printf("%d iterations had an inappropriate number of card draws for the current player.\n", wrongHandCount);
+    printf("End random testing of minion card.\n\n");
     return 0;
 }
--- projects/roquej/dominion/randomtestcard3.c
+++ projects/roquej/dominion/randomtestcard3.c
@@ -4,11 +4,105 @@
 // random test generator- tribute card
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include <string.h>
 #include "dominion.h"
 #include "dominion_helpers.h"
 #include "rngs.h"
 
 int main(int argc, char* argv[]) {
+    printf("Begin random testing of tribute card:\n");
+
+    srand(time(NULL));
+    
+    // declare variables
+    int currentPlayer, nextPlayer, numPlayers,
+        preActions, postActions, preCoins, postCoins, preCards, postCards;
+    int revealedCards[2];
+    int hasEmptyDeck = 0;
+    int numActions = 0;
+    int numCoins = 0;
+    int numCards = 0;
+    int wrongActions = 0;
+    int wrongCoins = 0;
+    int wrongCards = 0;
+    int firstCard;
+    int secondCard;
+    struct gameState g0, g;
+    int k[10] = {minion, council_room, feast, gardens, mine,
+                 remodel, smithy, village, adventurer, great_hall};
+
+    for(int i = 0; i < 10000; i++) {
+        memcpy(&g, &g0, sizeof(struct gameState));
+        numActions = 0;
+        numCoins = 0;
+        numCards = 0;
+        numPlayers = 2 + rand() % 3;
+        initializeGame(numPlayers, k, 1000, &g);
+        currentPlayer = rand() % numPlayers;
+        nextPlayer = currentPlayer + 1;
+        if(nextPlayer >= numPlayers) {
+            nextPlayer = 0;
+        }
+
+        firstCard = rand() % 27;
+        gainCard(firstCard, &g, 1, nextPlayer);
+        secondCard = rand() % 27;
+        gainCard(secondCard, &g, 1, nextPlayer);
+
+        if(rand() % 100 < 10) {
+            g.deckCount[nextPlayer] = 0;
+            hasEmptyDeck = 1;
+        }
+        if(rand() % 100 < 10) {
+            gainCard(rand() % 27, &g, 0, nextPlayer);
+        } 
+
+        preActions = g.numActions;
+        preCoins = g.coins;
+        preCards = g.handCount[currentPlayer];
+
+        playTribute(revealedCards, currentPlayer, nextPlayer, &g);
+
+        postActions = g.numActions;
+        postCoins = g.coins;
+        postCards = g.handCount[currentPlayer];
+
+        if(!hasEmptyDeck) {
+            if(firstCard == copper || firstCard == silver || firstCard == gold) {
+                numCoins += 2;
+            } else if(firstCard == estate || firstCard == duchy || firstCard == province || firstCard == gardens || firstCard == great_hall) {
+                numCards += 2;
+            } else {
+                numActions += 2;
+            }
+            if(firstCard != secondCard) {
+                if(secondCard == copper || secondCard == silver || secondCard == gold) {
+                    numCoins += 2;
+                } else if(secondCard == estate || secondCard == duchy || secondCard == province || secondCard == gardens || secondCard == great_hall) {
+                    numCards += 2;
+                } else {
+                    numActions += 2;
+                }
+            }
+        }
+
+        if(preActions + numActions != postActions) {
+            wrongActions++;
+        }
+        if(preCoins + numCoins != postCoins) {
+            wrongCoins++;
+        }
+        if(preCards + numCards != postCards) {
+            wrongCards ++;
+        }
+    }
+
+    printf("In 10000 iterations:\n");
+    printf("%d iterations had an inappropriate change in actions.\n", wrongActions);
+    printf("%d iterations had an inappropriate change in coins.\n", wrongCoins);
+    printf("%d iterations had an inappropriate number of card draws for the current player.\n", wrongCards);
+    printf("End random testing of tribute card.\n\n");
     return 0;
 }
