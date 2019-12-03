13defbe236c202b690921fd24a8473f394090c02
Jonasel Roque <jonaselroque@me.com>
2019-12-03 12:13:48 -0800

    Update unittest2.c

--- projects/FinalProject-Bugs/dominion/unittest2.c
+++ projects/FinalProject-Bugs/dominion/unittest2.c
@@ -49,39 +49,6 @@ int main(int argc, char *argv[]) {
     } else {
         printf("Success: -1 was returned when attempting to trash a copper for a gold.\n");
     }
-    
-    // testing trashing a silver for a copper
-    memcpy(&G, &G0, sizeof(struct gameState));
-    initializeGame(2, k, 1000, &G);
-    G.hand[0][0] = silver;
-    G.hand[0][1] = mine;
-    G.handCount[0] = 2;
-    // assert silver and mine cards are in first player's hand
-    countSilver = 0;
-    countMine = 0;
-    for(int i = 0; i < G.handCount[0]; i++) {
-        if(G.hand[0][i] == silver) {
-            countSilver++;
-        } else if(G.hand[0][i] == mine) {
-            countMine++;
-        }
-    }
-    if(countSilver == 0) {
-        printf("Testing error: silver card was never added to first player's hand.\n");
-    }
-    if(countMine == 0) {
-        printf("Testing error: mine card was never added to first player's hand.\n");
-    }
-
-    // call mine case of cardEffect
-    num = cardEffect(mine, 0, copper, 0, &G, 1, b);
-    
-    // assertion
-    if(num == -1) {
-        printf("Bug found: -1 was incorrectly returned when attempting to trash a silver for a copper.\n");
-    } else {
-        printf("Success: -1 was not returned when attempting to trash a silver for a copper.\n");
-    }
 
     // testing trashing a gold for a silver
     memcpy(&G, &G0, sizeof(struct gameState));
@@ -146,7 +113,7 @@ int main(int argc, char *argv[]) {
     if(num == -1) {
         printf("Bug found: -1 was incorrectly returned when attempting to trash a gold for a copper.\n");
     } else {
-        printf("Success: -1 was not returned when attempting to trash a gold for a silver.\n");
+        printf("Success: -1 was not returned when attempting to trash a gold for a copper.\n");
     }
 
     printf("Bug #2 Unit Test completed.\n\n");
